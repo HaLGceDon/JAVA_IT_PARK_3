ed36b0b70cea11974637662e545a1643eb93d230
Student9 <Zaq12wsx>
2017-09-25 21:01:09 +0300

    add classwork

---
+++ Tasks/Task3/src/ru/itpark/Main.java
@@ -0,0 +1,13 @@
+package ru.itpark;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        char s = (char)n;
+        System.out.println(s);
+    }
+}
